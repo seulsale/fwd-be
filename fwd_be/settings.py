32e5807f66e8272922e44755abfb46434062cfcf
Sergio Saucedo <sergio.saucedo10@gmail.com>
2019-11-20 22:46:54 -0600

    Add django heroku for db setup

--- fwd_be/settings.py
+++ fwd_be/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django-heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -136,3 +137,5 @@ STATICFILES_DIRS = (
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+django_heroku.settings(locals())

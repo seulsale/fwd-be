bcebe5ffb0e3e2537a8ac093fdeba47a12c3c28c
Sergio Saucedo <sergio.saucedo10@gmail.com>
2019-11-23 03:40:49 -0600

    db

--- fwd_be/settings.py
+++ fwd_be/settings.py
@@ -83,10 +83,9 @@ WSGI_APPLICATION = 'fwd_be.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
 
 
